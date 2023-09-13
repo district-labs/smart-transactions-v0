efb0f0f7b7b631a91b45697fcd243ad46bea16d4
Vitor <marthendalnunes@gmail.com>
2023-09-13 00:58:53 -0300

    feat: add extra tests to Twap Intent

--- contracts/intentify/test/TwapIntent.t.sol
+++ contracts/intentify/test/TwapIntent.t.sol
@@ -27,18 +27,79 @@ contract TwapIntentTest is PRBTest, StdCheats {
     // DAI/ETH on Uniswap V3 Ethereum Mainnet
     address immutable UNISWAP_V3_POOL = 0x60594a405d53811d3BC4766596EFD80fd545A270;
 
+    uint256 SIGNER = 0xA11CE;
+    address internal signer;
+    
+    Signature EMPTY_SIGNATURE = Signature({
+        r: bytes32(0x00),
+        s: bytes32(0x00),
+        v: uint8(0x00)
+    });
+    Hook EMPTY_HOOK = Hook({
+        target: address(0x00),
+        data: bytes("")
+    });
 
     /// @dev A function invoked before each test case is run.
     function setUp() public virtual {
         mainnetFork = vm.createFork(MAINNET_RPC_URL);
         vm.selectFork(mainnetFork);
         vm.rollFork(MAINNET_FORK_BLOCK);
+
+        // Instantiate the contract-under-test.
+        signer = vm.addr(SIGNER);
+        _intentify = new Intentify(signer, "Intentify", "V0");
         _twapIntent = new TwapIntentHarness();
     }
 
     /* ===================================================================================== */
     /* Success                                                                               */
     /* ===================================================================================== */
+
+    function test_TwapIntent_Success() external {
+        uint256 minPriceX96 = 49573475736131303867109805;
+
+        Intent[] memory intents = new Intent[](1);
+        intents[0] = Intent({
+            exec: IntentExecution({
+                root: address(_intentify),
+                target: address(_twapIntent),
+                data:  abi.encode(
+                    UNISWAP_V3_POOL,
+                    uint32(100),
+                    minPriceX96
+                )
+            }),
+            signature: EMPTY_SIGNATURE
+        });
+
+        IntentBatch memory intentBatch = IntentBatch({
+            nonce: DimensionalNonce({
+                queue: 0,
+                accumulator: 1
+            }),
+            intents: intents
+        });
+
+        bytes32 digest = _intentify.getIntentBatchTypedDataHash(intentBatch);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(SIGNER, digest);
+
+        Hook[] memory hooks = new Hook[](1);
+        hooks[0] = EMPTY_HOOK;
+
+        IntentBatchExecution memory batchExecution = IntentBatchExecution({
+            batch: intentBatch,
+            signature: Signature({
+                r: r,
+                s: s,
+                v: v
+            }),
+            hooks: hooks
+        });
+
+        bool _executed = _intentify.execute(batchExecution);
+        assertEq(true, _executed);
+    }
 
     function test_getTwapX96_ZeroInterval_Success() external {
         uint256 priceX96 = _twapIntent.exposed_getTwapX96(UNISWAP_V3_POOL, 0);
@@ -54,5 +115,93 @@ contract TwapIntentTest is PRBTest, StdCheats {
     /* Failing                                                                               */
     /* ===================================================================================== */
 
+    function test_RevertWhen_TwapIntent_PriceTooLow() external {
+        uint256 minPriceX96 = 49573475736131303867109806;
 
+        Intent[] memory intents = new Intent[](1);
+        intents[0] = Intent({
+            exec: IntentExecution({
+                root: address(_intentify),
+                target: address(_twapIntent),
+                data:  abi.encode(
+                    UNISWAP_V3_POOL,
+                    uint32(100),
+                    minPriceX96
+                )
+            }),
+            signature: EMPTY_SIGNATURE
+        });
+
+        IntentBatch memory intentBatch = IntentBatch({
+            nonce: DimensionalNonce({
+                queue: 0,
+                accumulator: 1
+            }),
+            intents: intents
+        });
+
+        bytes32 digest = _intentify.getIntentBatchTypedDataHash(intentBatch);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(SIGNER, digest);
+
+        Hook[] memory hooks = new Hook[](1);
+        hooks[0] = EMPTY_HOOK;
+
+        IntentBatchExecution memory batchExecution = IntentBatchExecution({
+            batch: intentBatch,
+            signature: Signature({
+                r: r,
+                s: s,
+                v: v
+            }),
+            hooks: hooks
+        });
+
+        vm.expectRevert(bytes("TwapIntent:low-price"));
+        _intentify.execute(batchExecution);
+    }
+
+    function test_RevertWhen_TwapIntent_InvalidRoot() external {
+        uint256 minPriceX96 = 49573475736131303867109805;
+
+        Intent[] memory intents = new Intent[](1);
+        intents[0] = Intent({
+            exec: IntentExecution({
+                root: address(0),
+                target: address(_twapIntent),
+                data:  abi.encode(
+                    UNISWAP_V3_POOL,
+                    uint32(100),
+                    minPriceX96
+                )
+            }),
+            signature: EMPTY_SIGNATURE
+        });
+
+        IntentBatch memory intentBatch = IntentBatch({
+            nonce: DimensionalNonce({
+                queue: 0,
+                accumulator: 1
+            }),
+            intents: intents
+        });
+
+        bytes32 digest = _intentify.getIntentBatchTypedDataHash(intentBatch);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(SIGNER, digest);
+
+        Hook[] memory hooks = new Hook[](1);
+        hooks[0] = EMPTY_HOOK;
+
+        IntentBatchExecution memory batchExecution = IntentBatchExecution({
+            batch: intentBatch,
+            signature: Signature({
+                r: r,
+                s: s,
+                v: v
+            }),
+            hooks: hooks
+        });
+
+        vm.expectRevert(bytes("TwapIntent:invalid-root"));
+        _intentify.execute(batchExecution);
+    }
 }
