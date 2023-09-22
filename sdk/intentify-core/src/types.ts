3dcea287499ea592724623874d2e7b4dac1d8dc2
Kames <sudo@kames.me>
2023-09-22 13:02:27 -0400

    fix: latest abis and types

--- sdk/intentify-core/src/types.ts
+++ sdk/intentify-core/src/types.ts
@@ -11,27 +11,27 @@ export type DimensionalNonce = {
 };
 
 export type Signature = {
-  r: string; // bytes32 can be represented as a hex string
-  s: string; // bytes32 can be represented as a hex string
+  r: `0x${string}`; // bytes32 can be represented as a hex string
+  s: `0x${string}`; // bytes32 can be represented as a hex string
   v: number; // uint8 can be represented as a number in TypeScript
 };
 
 export type Intent = {
-  readonly root: `0x{string}`; // Ethereum addresses are usually represented as strings in TypeScript
-  readonly target: `0x{string}`; // Ethereum addresses are usually represented as strings in TypeScript
+  readonly root: `0x${string}`; // Ethereum addresses are usually represented as strings in TypeScript
+  readonly target: `0x${string}`; // Ethereum addresses are usually represented as strings in TypeScript
   readonly value: bigint; // Ethereum addresses are usually represented as strings in TypeScript
-  readonly data: `0x{string}`; // bytes can be represented as a hex string
+  readonly data: `0x${string}`; // bytes can be represented as a hex string
 };
 
 export type IntentBatch = {
-  readonly root: `0x{string}`;
-  readonly nonce: `0x{string}`;
+  readonly root: `0x${string}`;
+  readonly nonce: `0x${string}`;
   readonly intents: Intent[];
 };
 
 export type Hook = {
-  target: string; // Ethereum addresses are usually represented as strings in TypeScript
-  data: string; // bytes can be represented as a hex string
+  target: `0x${string}`; // Ethereum addresses are usually represented as strings in TypeScript
+  data: `0x${string}`; // bytes can be represented as a hex string
 };
 
 export type IntentBatchExecution = {
