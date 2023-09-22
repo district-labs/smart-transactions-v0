7c828865eedcac10d6be324c951f88f019a11e78
Kames <sudo@kames.me>
2023-09-22 12:44:15 -0400

    feat: vercel.json

---
+++ apps/site/app/api/blockchain-clients.ts
@@ -0,0 +1,36 @@
+import { createWalletClient, http } from "viem"
+import type { WalletClient } from "viem"
+import { privateKeyToAccount } from "viem/accounts"
+import { goerli, mainnet } from "viem/chains"
+
+const account = privateKeyToAccount(
+  (process.env.PRIVATE_KEY as `0x{string}`) || "0x00"
+)
+
+const transportMainnet = http(
+  `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
+)
+const transportGoerli = http(
+  `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
+)
+
+export const mainnetWalletClient = createWalletClient({
+  account: account,
+  chain: mainnet,
+  transport: transportMainnet,
+})
+
+export const goerliWalletClient = createWalletClient({
+  account: account,
+  chain: goerli,
+  transport: transportGoerli,
+})
+
+type KeyList = {
+  [key: number]: WalletClient
+}
+
+export const walletClients: KeyList = {
+  1: mainnetWalletClient,
+  5: goerliWalletClient,
+}
---
+++ apps/site/db/queries/intent-batch.ts
@@ -0,0 +1,24 @@
+import { db } from ".."
+
+export const selectAllIntentBatchQuery = db.query.intentBatchRelations.findMany(
+  {
+    with: {
+      intentBatch: {
+        with: {
+          intents: true,
+        },
+      },
+    },
+  }
+)
+
+export type IntentBatchQuery = ReturnType<
+  typeof selectAllIntentBatchQuery.execute
+>
+
+export type SelectAllIntentBatchQuery = Awaited<
+  ReturnType<typeof selectAllIntentBatchQuery.execute>
+>
+export type SelectOneIntentBatchQuery = Awaited<
+  ReturnType<typeof selectAllIntentBatchQuery.execute>
+>[number]
