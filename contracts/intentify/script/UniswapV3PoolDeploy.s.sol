60179c3cbe0c8fb2caaff0fedc0ac8a54d41b072
Vitor <marthendalnunes@gmail.com>
2023-09-22 15:08:35 -0300

    fix: Uniswap V3 Pool deployment

--- contracts/intentify/script/UniswapV3PoolDeploy.s.sol
+++ contracts/intentify/script/UniswapV3PoolDeploy.s.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.19;
 
+import "../src/libraries/UniswapV3/SqrtMath.sol";
+import { UniswapV3Address } from "../src/libraries/UniswapV3/UniswapV3Address.sol";
 import { TickMath } from "@uniswap/v3-core/contracts/libraries/TickMath.sol";
 import { TransferHelper } from "v3-periphery/libraries/TransferHelper.sol";
 import { INonfungiblePositionManager } from "v3-periphery/interfaces/INonfungiblePositionManager.sol";
@@ -11,65 +13,72 @@ import "forge-std/Script.sol";
 import { ERC20Mintable } from "../src/periphery/ERC20Mintable.sol";
 
 contract UniswapV3PoolDeploy is Script {
-    address public immutable uniswapV3Factory = address(0x1F98431c8aD98523631AE4a59f267346ea31F984); // goerli
-    INonfungiblePositionManager nonfungiblePositionManager =
-        INonfungiblePositionManager(0xC36442b4a4522E871399CD717aBDD847Ab11FE88); // goerli
+    INonfungiblePositionManager public nonfungiblePositionManager =
+        INonfungiblePositionManager(UniswapV3Address.UNIV3_POS_MANAGER);
+    uint256 private deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    address private deployerAddress = vm.addr(deployerPrivateKey);
 
     function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        ERC20Mintable disUSDC = new ERC20Mintable("Test USDC","testUSDC",6);
-        ERC20Mintable disWETH = new ERC20Mintable("Test WETH","testWETH",18);
+        ERC20Mintable disUSDC = new ERC20Mintable("Test USDC", "testUSDC", 6);
+        ERC20Mintable disWETH = new ERC20Mintable("Test WETH", "testWETH", 18);
 
-        uint24 poolFee = uint24(3000);
-        uint160 sqrtPriceX96 = TickMath.getSqrtRatioAtTick(TickMath.MAX_TICK);
+        uint24 poolFee = 3000;
+        uint24 tickWidth = 2;
+        int24 tickSpacing;
+
+        uint256 amount0Desired = 1500e6;
+        uint256 amount1Desired = 1e18;
+
+        uint160 sqrtPriceX96 = encodePriceSqrt(amount1Desired, amount0Desired);
 
         address uniswapV3PoolAddress =
-            IUniswapV3Factory(uniswapV3Factory).createPool(address(disUSDC), address(disWETH), poolFee );
-
+            IUniswapV3Factory(UniswapV3Address.UNIV3_FACTORY).createPool(address(disUSDC), address(disWETH), poolFee);
         IUniswapV3Pool uniswapV3Pool = IUniswapV3Pool(uniswapV3PoolAddress);
         uniswapV3Pool.initialize(sqrtPriceX96);
 
-        // Mint to TESTING account
-        address metamask = 0xbcB93066f45d7e10DABBd233c4898509cf271216;
-        disUSDC.mint(metamask, 10_000e6);
-        disWETH.mint(metamask, 10_000e18);
+        tickSpacing = uniswapV3Pool.tickSpacing();
 
-        address executor = 0x983d2CbE5525cFf49c3d4F949AD6766c7d01244F;
-        // Mint to DEPLOYER address
-        disUSDC.mint(address(executor), 10_000e6);
-        disWETH.mint(address(executor), 10_000e18);
-        TransferHelper.safeApprove(address(disUSDC), address(nonfungiblePositionManager), 10_000e6);
-        TransferHelper.safeApprove(address(disWETH), address(nonfungiblePositionManager), 10_000e18);
-        // disUSDC.approve(address(nonfungiblePositionManager), type(uint256).max);
-        // disWETH.approve(address(nonfungiblePositionManager), type(uint256).max);
+        mintTokensAndApprove(disUSDC, disWETH, amount0Desired, amount1Desired);
 
-        address recipient = address(metamask);
-        uint256 amount0Desired = 1000;
-        uint256 amount1Desired = 1000;
+        (, int24 curTick,,,,,) = uniswapV3Pool.slot0();
+        curTick = curTick - (curTick % tickSpacing);
+
+        int24 lowerTick = curTick - (tickSpacing * int24(tickWidth));
+        int24 upperTick = curTick + (tickSpacing * int24(tickWidth));
 
         INonfungiblePositionManager.MintParams memory params = INonfungiblePositionManager.MintParams({
-            token0: address(disUSDC),
-            token1: address(disWETH),
+            token0: uniswapV3Pool.token0(),
+            token1: uniswapV3Pool.token1(),
             fee: poolFee,
-            tickLower: TickMath.MIN_TICK,
-            tickUpper: TickMath.MAX_TICK,
+            tickLower: lowerTick,
+            tickUpper: upperTick,
             amount0Desired: amount0Desired,
             amount1Desired: amount1Desired,
-            amount0Min: 0,
-            amount1Min: 0,
-            recipient: recipient,
-            deadline: block.timestamp
+            amount0Min: 0e6,
+            amount1Min: 0e18,
+            recipient: deployerAddress,
+            deadline: block.timestamp + 1000
         });
 
-        // int24 amount0 = 1;
-        // int24 amount1 = 100;
-
-        (uint256 tokenId, uint128 liquidity, uint256 amount0, uint256 amount1) = nonfungiblePositionManager.mint(params);
-
-        // TODO: add pool initialization
+        nonfungiblePositionManager.mint(params);
 
         vm.stopBroadcast();
+    }
+
+    function mintTokensAndApprove(
+        ERC20Mintable token0,
+        ERC20Mintable token1,
+        uint256 amount0Desired,
+        uint256 amount1Desired
+    )
+        internal
+    {
+        token0.mint(deployerAddress, amount0Desired);
+        token1.mint(deployerAddress, amount1Desired);
+
+        TransferHelper.safeApprove(address(token0), address(nonfungiblePositionManager), amount0Desired);
+        TransferHelper.safeApprove(address(token1), address(nonfungiblePositionManager), amount1Desired);
     }
 }
