2b96a08f1fd711ccb1237ea075972286f01a5a85
Vitor <marthendalnunes@gmail.com>
2023-09-26 18:09:05 -0300

    feat: add failling test

--- contracts/intentify/test/e2e/LimitOrderEngineHub.t.sol
+++ contracts/intentify/test/e2e/LimitOrderEngineHub.t.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.19 <0.9.0;
 
-import { console2 } from "forge-std/console2.sol";
 import { IERC20 } from "../../lib/openzeppelin-contracts/contracts/interfaces/IERC20.sol";
 import { ISwapRouter } from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
@@ -54,10 +53,17 @@ contract LimitOrderEngineHubTest is BaseTest {
 
     /// @dev A function invoked before each test case is run.
     function setUp() public virtual {
+        _initializeFork();
+        _initializeContracts();
+    }
+
+    function _initializeFork() private {
         _goerliFork = vm.createFork(_goerliRpcUrl);
         vm.selectFork(_goerliFork);
         vm.rollFork(_goerliForkBlock);
+    }
 
+    function _initializeContracts() private {
         initializeBase();
         _intentify = new Intentify(signer, "Intentify", "V0");
         _limitOrderIntent = new LimitOrderIntentHarness();
@@ -163,12 +169,117 @@ contract LimitOrderEngineHubTest is BaseTest {
 
         IntentBatchExecution memory batchExecution =
             IntentBatchExecution({ batch: intentBatch, signature: Signature({ r: r, s: s, v: v }), hooks: hooks });
-        console2.log("address(this) 2", address(this));
 
         bool _executed = _intentify.execute(batchExecution);
         assertEq(true, _executed);
 
         bool _unlocked = _limitOrderIntent.exposed_unlock(address(_intentify), address(_testWETH), address(_testUSDC));
         assertEq(true, _unlocked);
+    }
+
+    /* ===================================================================================== */
+    /* Failing                                                                               */
+    /* ===================================================================================== */
+
+    function test_RevertWhen_InsufficientTokenOutBalance() external {
+        // Place a limit order of 150 USDC for 0.11 WETH
+        uint256 amountInMaximum = 0.11e18;
+        uint256 amountOut = 150e6;
+        address tokenIn = address(_testWETH);
+        address tokenOut = address(_testUSDC);
+
+        _testWETH.mint(address(_intentify), amountInMaximum);
+        vm.prank(address(_intentify));
+        _testWETH.approve(address(_tokenRouterReleaseIntent), amountInMaximum);
+
+        Intent[] memory intents = new Intent[](2);
+
+        // ------------------------------------------------------
+        // Token Release Intent
+        // ------------------------------------------------------
+        intents[0] = Intent({
+            root: address(_intentify),
+            value: 0,
+            target: address(_tokenRouterReleaseIntent),
+            data: _tokenRouterReleaseIntent.encode(address(_testWETH), amountInMaximum)
+        });
+
+        // ------------------------------------------------------
+        // Limit Order Intent
+        // ------------------------------------------------------
+
+        intents[1] = Intent({
+            root: address(_intentify),
+            value: 0,
+            target: address(_limitOrderIntent),
+            data: _limitOrderIntent.encode(address(_testWETH), address(_testUSDC), amountInMaximum, amountOut)
+        });
+
+        IntentBatch memory intentBatch =
+            IntentBatch({ root: address(_intentify), nonce: abi.encodePacked(uint256(0)), intents: intents });
+
+        bytes32 digest = _intentify.getIntentBatchTypedDataHash(intentBatch);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(SIGNER, digest);
+
+        Hook[] memory hooks = new Hook[](2);
+        hooks[0] = EMPTY_HOOK;
+
+        // ------------------------------------------------------
+        // Engine Hub Call
+        // ------------------------------------------------------
+
+        address[] memory engineHubTargets = new address[](3);
+        bytes[] memory engineHubdata = new bytes[](3);
+
+        // Claim the WETH from the TokenRouterReleaseIntent
+        // The WETH is now transferred to the EngineHub
+        engineHubTargets[0] = address(_tokenRouterReleaseIntent);
+        engineHubdata[0] = abi.encodeWithSignature(
+            "claim(address,address,address,uint256)",
+            address(_intentify),
+            address(_engineHub),
+            address(_testWETH),
+            amountInMaximum
+        );
+
+        // Approve the WETH from the EngineHub to the SwapRouter
+        engineHubTargets[1] = address(_testWETH);
+        engineHubdata[1] = abi.encodeWithSignature("approve(address,uint256)", address(_swapRouter), amountInMaximum);
+
+        // Swap the WETH for USDC and transfer the USDC to the Intentify contract
+        // The remaining WETH is kept in the EngineHub as a payment for the operation
+        engineHubTargets[2] = address(_swapRouter);
+        engineHubdata[2] = abi.encodeWithSignature(
+            "exactOutputSingle((address,address,uint24,address,uint256,uint256,uint256,uint160))",
+            ISwapRouter.ExactOutputSingleParams({
+                tokenIn: tokenIn,
+                tokenOut: tokenOut,
+                fee: poolFee,
+                recipient: address(_intentify),
+                deadline: block.timestamp,
+                // If the amountOut of the swap is smaller than the amountOut of
+                // the limit order, the swap will fail.
+                // We simulate this by dividing the amountOut by 2.
+                amountOut: amountOut / 2,
+                amountInMaximum: amountInMaximum,
+                sqrtPriceLimitX96: 0
+            })
+        );
+
+        // Encode the EngineHub multiCall in a single hook call
+        hooks[1] = Hook({
+            target: address(_engineHub),
+            data: abi.encodeWithSignature("multiCall(address[],bytes[])", engineHubTargets, engineHubdata)
+        });
+
+        // ------------------------------------------------------
+        // Intent Batch Execution
+        // ------------------------------------------------------
+
+        IntentBatchExecution memory batchExecution =
+            IntentBatchExecution({ batch: intentBatch, signature: Signature({ r: r, s: s, v: v }), hooks: hooks });
+
+        vm.expectRevert("LimitOrderIntent:unlock:tokenIn:insufficient-balance");
+        _intentify.execute(batchExecution);
     }
 }
