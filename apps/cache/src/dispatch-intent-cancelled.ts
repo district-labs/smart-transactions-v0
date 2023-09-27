cf3f97aae78c102a851ca6b12b1a988c44b30a69
Vitor <marthendalnunes@gmail.com>
2023-09-27 16:58:11 -0300

    fix: event handlers

--- apps/cache/src/dispatch-intent-cancelled.ts
+++ apps/cache/src/dispatch-intent-cancelled.ts
@@ -1,7 +1,8 @@
-const siteUrl = process.env.SITE_URL
-if(!siteUrl) throw new Error("SITE_URL not set")
+
 
 export async function dispatchIntentCancelled(chainId: number, intentBatchId: `0x${string}`, transactionHash: `0x${string}`){
+    const siteUrl = process.env.SITE_URL
+    if(!siteUrl) throw new Error("SITE_URL not set")
     const res = await fetch(`${siteUrl}/api/events/cancelled`, {
         method: 'POST',
         headers: {
--- apps/cache/src/dispatch-intent-execution.ts
+++ apps/cache/src/dispatch-intent-execution.ts
@@ -1,7 +1,9 @@
-const siteUrl = process.env.SITE_URL
-if(!siteUrl) throw new Error("SITE_URL not set")
+
+
 
 export async function dispatchIntentExecution(chainId: number, intentBatchId: `0x${string}`, transactionHash: `0x${string}`){
+    const siteUrl = process.env.SITE_URL
+    if(!siteUrl) throw new Error("SITE_URL not set")
     const res = await fetch(`${siteUrl}/api/events/executed`, {
         method: 'POST',
         headers: {
