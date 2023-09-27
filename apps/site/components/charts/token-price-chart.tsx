a832a68dfe5d604c431b8bd4aad023491d3473c5
Justin Bassey <101466555+0xJBass@users.noreply.github.com>
2023-09-27 12:44:45 -0400

    Site update charting library dis 246 (#36)
    * feat: switch chart to recharts
    
    * fix: build errors
    
    * cleanup
    
    * fix: lock
    
    ---------
    
    Co-authored-by: Kames Geraghty <sudo@kames.me>

--- apps/site/components/charts/token-price-chart.tsx
+++ apps/site/components/charts/token-price-chart.tsx
@@ -1,10 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 "use client"
 
 import { useState } from "react"
 import { type DefiLlamaToken } from "@/types"
 import { useQuery } from "@tanstack/react-query"
-import { LineChart } from "@tremor/react"
+import {
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+  type TooltipProps,
+} from "recharts"
+import {
+  type NameType,
+  type ValueType,
+} from "recharts/types/component/DefaultTooltipContent"
 
+import { cn, formatDate } from "@/lib/utils"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card"
 import ChartLoadingSkeleton from "./chart-loading-skeleton"
 import {
   ChartTimeFilters,
@@ -44,13 +66,20 @@ export default function TokenPriceChart({
                 address: outToken.address,
               },
             ],
-            period: chartRange,
-            spanDataPoints: 50,
+            range: chartRange,
           }),
           headers: {
             "Content-Type": "application/json",
           },
-        }).then((res) => res.json())
+        }).then(
+          (res) =>
+            res.json() as Promise<
+              {
+                time: number
+                price: number
+              }[]
+            >
+        )
       },
       enabled: !!inToken && !!outToken,
     }
@@ -58,45 +87,117 @@ export default function TokenPriceChart({
 
   if (status === "error") return <p>Failed to load chart</p>
 
-  return (
-    <>
-      <div className="flex w-full items-center justify-between">
-        <div className="flex items-center space-x-2">
-          {outToken && inToken && (
-            <div className="flex -space-x-1">
-              <img
-                src={outToken?.logoURI}
-                alt={outToken.name}
-                className="inline-block h-8 w-8 rounded-full ring-2 ring-background"
-              />
-              <img
-                src={inToken?.logoURI}
-                alt={inToken.name}
-                className="inline-block h-8 w-8 rounded-full ring-2 ring-background"
-              />
-            </div>
-          )}
-          <h2 className="text-2xl font-medium tracking-tight">
-            {`${inToken?.symbol}/${outToken?.symbol}`}
-          </h2>
+  const CustomTooltip = ({
+    active,
+    payload,
+  }: TooltipProps<ValueType, NameType>) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className="w-30 rounded border bg-background px-3 py-2 text-center">
+          <p className="font-medium">{payload[0].payload.price.toFixed(2)}</p>
+          <p className="text-xs text-muted-foreground">
+            {formatDate(payload[0].payload.time, {
+              year: "numeric",
+            })}
+          </p>
+          <p className="text-xs text-muted-foreground">
+            {`${new Date(payload[0].payload.time * 1000).getHours()}:00`}
+          </p>
         </div>
-        <ChartTimeFilters
-          range={chartRange}
-          setRange={setChartRange}
-          refetch={refetch}
-        />
-      </div>
-      {status === "loading" ? (
-        <ChartLoadingSkeleton />
-      ) : (
-        <LineChart
-          data={data}
-          index="time"
-          categories={["price"]}
-          yAxisWidth={64}
-          autoMinValue={true}
-        />
-      )}
-    </>
+      )
+    }
+    return null
+  }
+
+  let percentage
+
+  if (status === "success" && data) {
+    percentage = (
+      ((data.slice(-1)[0].price - data.slice(0)[0].price) /
+        data.slice(0)[0].price) *
+      100
+    ).toFixed(2)
+
+    if (parseFloat(percentage) > 0) {
+      percentage = "+" + percentage
+    }
+  }
+
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex w-full items-center justify-between">
+          <div className="flex items-center space-x-2">
+            {outToken && inToken && (
+              <div className="flex -space-x-1">
+                <img
+                  src={outToken?.logoURI}
+                  alt={outToken.name}
+                  className="inline-block h-8 w-8 rounded-full ring-2 ring-background"
+                />
+                <img
+                  src={inToken?.logoURI}
+                  alt={inToken.name}
+                  className="inline-block h-8 w-8 rounded-full ring-2 ring-background"
+                />
+              </div>
+            )}
+            <CardTitle className="text-2xl font-semibold tracking-tight">{`${inToken?.symbol}/${outToken?.symbol}`}</CardTitle>
+          </div>
+          <ChartTimeFilters
+            range={chartRange}
+            setRange={setChartRange}
+            refetch={refetch}
+          />
+        </div>
+        <CardDescription>
+          {status === "success" && (
+            <>
+              <h2 className="text-xl font-medium text-foreground">
+                {data && data.slice(-1)[0].price.toFixed(2)}
+              </h2>
+              {percentage && (
+                <p
+                  className={cn(
+                    "text-muted-foreground",
+                    parseFloat(percentage) > 0 && "text-green-600",
+                    parseFloat(percentage) < 0 && "text-destructive"
+                  )}
+                >
+                  {percentage}%
+                </p>
+              )}
+            </>
+          )}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        {status === "loading" ? (
+          <ChartLoadingSkeleton />
+        ) : (
+          <ResponsiveContainer width="100%" height={300} minHeight={250}>
+            <LineChart width={500} height={300} data={data}>
+              <Line
+                type="monotone"
+                dataKey="price"
+                dot={false}
+                stroke="green"
+              />
+              <XAxis
+                dataKey="time"
+                tickMargin={5}
+                tickLine={false}
+                domain={["auto", "auto"]}
+                interval="preserveStartEnd"
+                tickFormatter={(value) => formatDate(value, {})}
+                minTickGap={24}
+              />
+              <YAxis domain={["auto", "auto"]} orientation="right" hide />
+              <Tooltip content={<CustomTooltip />} />
+            </LineChart>
+          </ResponsiveContainer>
+        )}
+      </CardContent>
+    </Card>
   )
 }
