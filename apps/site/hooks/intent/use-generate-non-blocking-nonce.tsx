0378840a8f82165a351123fc0acf8ec5c5d57593
Kames <sudo@kames.me>
2023-09-28 06:19:28 -0400

    feat: use bigint bitwise

--- apps/site/hooks/intent/use-generate-non-blocking-nonce.tsx
+++ apps/site/hooks/intent/use-generate-non-blocking-nonce.tsx
@@ -1,14 +1,16 @@
 import { encodeDimensionalNonce } from "@district-labs/intentify-utils";
-import { BigNumber } from "ethers";
 import type { Hex } from "viem";
 
-// const UINT32MAX = BigNumber.from('4294967295');
-const UINT120MAX = BigNumber.from('2').pow(120).sub(1);
-const UINT128MAX = BigNumber.from('2').pow(128).sub(1);
+// (BigInt(2) ** BigInt(120)) - BigInt(1);
+const UINT120MAX = BigInt('1329227995784915872903807060280344575')
 
-export function useGenerateNonBlockingNonce(salt: Hex): `0x${string}` {
-    const queue = BigInt(BigNumber.from(salt).and(UINT120MAX).toHexString())
-    const nonce = BigInt(BigNumber.from(0).and(UINT128MAX).toHexString())
+// (BigInt(2) ** BigInt(128)) - BigInt(1);
+const UINT128MAX = BigInt('340282366920938463463374607431768211455')
+
+export function useGenerateNonBlockingNonce(queueSalt: Hex): `0x${string}` {
+    const queue = BigInt(queueSalt) & UINT120MAX;
+    const nonce = BigInt(0) & UINT128MAX;
+    
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
     return encodeDimensionalNonce(queue, nonce)
-} 
+}
