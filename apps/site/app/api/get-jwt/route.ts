d0527d89ed5584c60dd2ac7bb0d4e97066970ae7
Vitor <marthendalnunes@gmail.com>
2023-09-28 14:04:27 -0300

    feat: move app admin auth to middleware

--- apps/site/app/api/get-jwt/route.ts
+++ apps/site/app/api/get-jwt/route.ts
@@ -1,46 +1,40 @@
-import { env } from "@/env.mjs";
-import { roles, signJwt, verifyJwt } from "@/lib/jwt";
-import { ironOptions } from "@/lib/session";
-import { getIronSession } from "iron-session";
-
-const admins = env.APP_ADMINS?.split(",") || []
+import { roles, signJwt, verifyJwt } from "@/lib/jwt"
 
 export async function GET(req: Request): Promise<Response> {
-    // Only app admins can access this endpoint
-    const res = new Response();
-	const session = await getIronSession(req, res, ironOptions);
+  // Extract role from URL
+  const url = new URL(req.url)
+  const role = url.searchParams.get("role")
 
-    if(!session?.user?.address || !admins.includes(session.user.address)) {
-        return new Response(JSON.stringify({
-            error: "Unauthorized",
-        }), { status: 401 });
-    }
+  // Validate role
+  if (!role || !roles?.includes(role)) {
+    return new Response(
+      JSON.stringify({
+        error: "Invalid or missing 'role' parameter",
+      }),
+      { status: 422 }
+    )
+  }
 
-    // Extract role from URL
-    const url = new URL(req.url);
-    const role = url.searchParams.get("role");
+  // Create payload and sign it
+  const payload = {
+    id: "engine-runner",
+    url: "https://engine.llama.fi",
+    roles: [role],
+  }
+  const jwt = await signJwt(payload)
 
-    // Validate role
-    if (!role || !roles?.includes(role)) {
-        return new Response(JSON.stringify({
-            error: "Invalid or missing 'role' parameter",
-        }), { status: 422 });
-    }
+  // Verify JWT
+  const verifiedPayload = await verifyJwt(jwt)
 
-    // Create payload and sign it
-    const payload = {
-        id: "engine-runner",
-        url: "https://engine.llama.fi",
-        roles: [role],
-    };
-    const jwt = await signJwt(payload);
-
-    // Verify JWT
-    const verifiedPayload = await verifyJwt(jwt);
-
-    // Return response
-    return new Response(JSON.stringify({
+  // Return response
+  return new Response(
+    JSON.stringify(
+      {
         jwt,
         verifiedPayload,
-    }, null, 2));
+      },
+      null,
+      2
+    )
+  )
 }
--- apps/site/middleware.ts
+++ apps/site/middleware.ts
@@ -1,92 +1,128 @@
-import { verifyJwt } from "@/lib/jwt";
-import { ironOptions } from "@/lib/session";
-import { getIronSession } from "iron-session/edge";
-import { headers } from "next/headers";
-import { NextResponse, type NextRequest } from "next/server";
+import { env } from "@/env.mjs"
+import { getIronSession } from "iron-session/edge"
+import { headers } from "next/headers"
+import { NextResponse, type NextRequest } from "next/server"
+
+import { verifyJwt } from "@/lib/jwt"
+import { ironOptions } from "@/lib/session"
+
+const appAdmins = env.APP_ADMINS?.split(",") || []
 
 // Constants
 const ROLES = {
-	ENGINE: "ENGINE",
-	ADMIN: "ADMIN",
-	CACHE: "CACHE",
-};
-const ENDPOINTS = {
-	ENGINE: ["/api/engine", "/api/execute"],
-	EVENTS: ["/api/events"],
-};
+  ENGINE: "ENGINE",
+  ADMIN: "ADMIN",
+  CACHE: "CACHE",
+}
 
-const JWT_PROTECTED_ROUTES = [
-	"/api/engine",
-	"/api/events",
-	"/api/execute",
-];
+const ENDPOINTS = {
+  ENGINE: ["/api/engine", "/api/execute"],
+  EVENTS: ["/api/events"],
+}
+
+const JWT_PROTECTED_ROUTES = ["/api/engine", "/api/events", "/api/execute"]
 
 const USER_SESSION_PROTECTED_ROUTES = [
-	"/api/admin",
-	"/api/intent-batch",
-	"/api/intent-batch/execution",
-	"/api/strategy",
-	"/api/token",
-];
+  "/api/admin",
+  "/api/intent-batch",
+  "/api/intent-batch/execution",
+  "/api/strategy",
+  "/api/token",
+]
+
+const APP_ADMIN_PROTECTED_ROUTES = ["/api/get-jwt"]
 
 // Helper function to generate unauthorized response
 function unauthorizedResponse(
-	message = "You are not authorized to access this resource",
+  message = "You are not authorized to access this resource"
 ) {
-	return new NextResponse(JSON.stringify({ error: message }), { status: 401 });
+  return new NextResponse(JSON.stringify({ error: message }), { status: 401 })
 }
 
 export async function middleware(request: NextRequest) {
-	const pathname = request.nextUrl.pathname;
+  const pathname = request.nextUrl.pathname
 
-	if (JWT_PROTECTED_ROUTES.some(route => pathname.startsWith(route))) {
-		return await handleJwtProtection(request, pathname);
-	}
+  if (JWT_PROTECTED_ROUTES.some((route) => pathname.startsWith(route))) {
+    return await handleJwtProtection(pathname)
+  }
 
-	if (USER_SESSION_PROTECTED_ROUTES.some(route => pathname.startsWith(route))) {
-		return await handleUserSessionProtection(request);
-	}
+  if (
+    USER_SESSION_PROTECTED_ROUTES.some((route) => pathname.startsWith(route))
+  ) {
+    return await handleUserSessionProtection(request)
+  }
 
-	// Continue to the next middleware or request handler
-	return NextResponse.next();
+  if (APP_ADMIN_PROTECTED_ROUTES.some((route) => pathname.startsWith(route))) {
+    return await handleAppAdminProtection(request)
+  }
+
+  // Continue to the next middleware or request handler
+  return NextResponse.next()
 }
 
 async function handleUserSessionProtection(request: NextRequest) {
-	const res = new Response();
-	const session = await getIronSession(request, res, ironOptions);
-	if (!session?.user?.address) {
-		return unauthorizedResponse("No user session");
-	}
+  const res = new Response()
+  const session = await getIronSession(request, res, ironOptions)
+  if (!session?.user?.address) {
+    return unauthorizedResponse("No user session")
+  }
 
-	return NextResponse.next();
+  return NextResponse.next()
 }
 
-async function handleJwtProtection(request: NextRequest, pathname: string) {
-	const headersInstance = headers();
-	const authorization = headersInstance.get("authorization");
+async function handleAppAdminProtection(request: NextRequest) {
+  const res = new Response()
+  const session = await getIronSession(request, res, ironOptions)
+  if (!session?.user?.address || !appAdmins.includes(session.user.address)) {
+    return unauthorizedResponse("Insufficient privileges")
+  }
 
-	if (!authorization) {
-		return unauthorizedResponse("No authorization header");
-	}
-
-	const payload = await verifyJwt(authorization);
-	if (!payload || !payload.roles) {
-		return unauthorizedResponse("Invalid token");
-	}
-
-	// Authorization checks
-	if (isAuthorized(pathname, payload.roles, ENDPOINTS.ENGINE, [ROLES.ENGINE, ROLES.ADMIN])) {
-		return NextResponse.next();
-	}
-
-	if (isAuthorized(pathname, payload.roles, ENDPOINTS.EVENTS, [ROLES.CACHE, ROLES.ADMIN])) {
-		return NextResponse.next();
-	}
-
-	return unauthorizedResponse();
+  return NextResponse.next()
 }
 
-function isAuthorized(pathname: string, roles: string[], endpoints: string[], requiredRoles: string[]): boolean {
-	return endpoints.some(endpoint => pathname.startsWith(endpoint)) && 
-		roles.some(role => requiredRoles.includes(role));
+async function handleJwtProtection(pathname: string) {
+  const headersInstance = headers()
+  const authorization = headersInstance.get("authorization")
+
+  if (!authorization) {
+    return unauthorizedResponse("No authorization header")
+  }
+
+  const payload = await verifyJwt(authorization)
+  if (!payload || !payload.roles) {
+    return unauthorizedResponse("Invalid token")
+  }
+
+  // Authorization checks
+  if (
+    isAuthorized(pathname, payload.roles, ENDPOINTS.ENGINE, [
+      ROLES.ENGINE,
+      ROLES.ADMIN,
+    ])
+  ) {
+    return NextResponse.next()
+  }
+
+  if (
+    isAuthorized(pathname, payload.roles, ENDPOINTS.EVENTS, [
+      ROLES.CACHE,
+      ROLES.ADMIN,
+    ])
+  ) {
+    return NextResponse.next()
+  }
+
+  return unauthorizedResponse()
+}
+
+function isAuthorized(
+  pathname: string,
+  roles: string[],
+  endpoints: string[],
+  requiredRoles: string[]
+): boolean {
+  return (
+    endpoints.some((endpoint) => pathname.startsWith(endpoint)) &&
+    roles.some((role) => requiredRoles.includes(role))
+  )
 }
