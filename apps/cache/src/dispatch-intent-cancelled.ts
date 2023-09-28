83f20d52f77c435d37c63e6c97ab674b8b1f8898
Vitor Marthendal Nunes <marthendalnunes@gmail.com>
2023-09-28 07:21:53 -0300

    Fix: ponder deploy (#38)
    * fix: ponder deploy
    
    * feat: add prod and dev envs
    
    * fix: add pnpm as package manager
    
    * refactor: add abis locally on cache
    
    * chore: add .nvmrc
    
    * fix: event handlers
    
    * fix: add error handler to event handlers

--- apps/cache/src/dispatch-intent-cancelled.ts
+++ apps/cache/src/dispatch-intent-cancelled.ts
@@ -1,13 +1,19 @@
+const siteUrl = process.env.SITE_URL
+if(!siteUrl) throw new Error("SITE_URL not set")
 export async function dispatchIntentCancelled(chainId: number, intentBatchId: `0x${string}`, transactionHash: `0x${string}`){
-    const res = await fetch(`http://localhost:3000/api/events/cancelled`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            chainId,
-            intentBatchId,
-            transactionHash
+    try {
+        await fetch(`${siteUrl}/api/events/cancelled`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                chainId,
+                intentBatchId,
+                transactionHash
+            })
         })
-    })
+    } catch (error) {
+        console.log(error)
+    }
 }
--- apps/cache/src/dispatch-intent-execution.ts
+++ apps/cache/src/dispatch-intent-execution.ts
@@ -1,13 +1,21 @@
+const siteUrl = process.env.SITE_URL
+if(!siteUrl) throw new Error("SITE_URL not set")
+
 export async function dispatchIntentExecution(chainId: number, intentBatchId: `0x${string}`, transactionHash: `0x${string}`){
-    const res = await fetch(`http://localhost:3000/api/events/executed`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            chainId,
-            intentBatchId,
-            transactionHash
+    try {
+        await fetch(`${siteUrl}/api/events/executed`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                chainId,
+                intentBatchId,
+                transactionHash
+            })
         })
-    })
+    } catch (error) {
+        console.log(error)
+    }
+  
 }
