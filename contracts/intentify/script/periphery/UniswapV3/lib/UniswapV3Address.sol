94570c07b532254f9c9ad4b35765b09b1e798bf5
Vitor <marthendalnunes@gmail.com>
2023-09-28 21:44:07 -0300

    feat: Univ3 scripts folder structure

---
+++ contracts/intentify/script/periphery/UniswapV3/lib/UniswapV3Address.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.19;
+
+library UniswapV3Address {
+    // Addresses
+    address public constant UNIV3_FACTORY = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
+    address public constant UNIV3_ROUTER = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+    address public constant UNIV3_POS_MANAGER = 0xC36442b4a4522E871399CD717aBDD847Ab11FE88;
+    address public constant UNIV3_QUOTER = 0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6;
+}
