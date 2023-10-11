b0d6e600aed597dc5c3d961d264a26809d12800f
Vitor <marthendalnunes@gmail.com>
2023-10-11 09:51:40 -0300

    feat: add timestamp to ponder cache

--- apps/uni-v3-twap-oracle-cache/src/UniswapV3TwapOracle.ts
+++ apps/uni-v3-twap-oracle-cache/src/UniswapV3TwapOracle.ts
@@ -1,17 +1,31 @@
 import { ponder } from "@/generated";
+import { createPublicClient, http } from 'viem';
+import { goerli } from 'viem/chains';
+
+const client = createPublicClient({ 
+  chain: goerli,
+  transport: http()
+})
 
 ponder.on("UniswapV3TwapOracle:ObservationStored", async ({event,context}) => {
-  const {UniswapV3Pool} = context.entities
+  const {UniswapV3Pool,BlockInfo } = context.entities
+
+  const block = await client.getBlock({
+    blockNumber: event.params.blockNumber
+  })
 
   await UniswapV3Pool.upsert({
     id: event.params.pool,
-    create: {
-      blocks: [
-        Number(event.params.blockNumber)
-      ]
-    },
-    update: ({current}) => ({
-     blocks:  [...current.blocks, Number(event.params.blockNumber)]
-    })
   })
+
+  await BlockInfo.create({
+    id: Number(event.params.blockNumber),
+    data: {
+      blockNumber: Number(event.params.blockNumber),
+      pool: event.params.pool,
+      timestamp: Number(block.timestamp),
+    }
+  })
+
+
 })
