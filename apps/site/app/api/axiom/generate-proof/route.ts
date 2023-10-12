501ea59737b5fd8cd4bf6f8f9335c3b1a415f1bf
Vitor <marthendalnunes@gmail.com>
2023-10-12 11:22:54 -0300

    chore: format site

---
+++ apps/site/app/api/axiom/generate-proof/route.ts
@@ -0,0 +1,74 @@
+import {
+  createPublicClient,
+  encodeFunctionData,
+  http,
+  type Address,
+} from "viem"
+import { goerli } from "viem/chains"
+
+import { getRelayerByChainId } from "@/lib/openzeppelin-defender/relayer"
+
+import { ax, UNI_V3_POOL_ADDR } from ".."
+import { axiomV1QueryABI } from "../abis"
+
+const chainId = 5
+
+export async function GET() {
+  try {
+    const client = createPublicClient({
+      chain: goerli,
+      transport: http(),
+    })
+    const currentBlockNumber = await client.getBlockNumber()
+    const blockNumber = Number(currentBlockNumber) - 1
+
+    const qb = ax.newQueryBuilder()
+    await qb.append({ blockNumber, address: UNI_V3_POOL_ADDR, slot: 8 })
+    const { keccakQueryResponse, query } = await qb.build()
+
+    const { relayer } = getRelayerByChainId(chainId)
+    const relayerAddress = (await relayer.getRelayer()).address as Address
+    const axiomV1QueryAddress = ax.getAxiomQueryAddress() as Address
+
+    const data = encodeFunctionData({
+      abi: axiomV1QueryABI,
+      functionName: "sendQuery",
+      args: [
+        keccakQueryResponse as `0x${string}`,
+        relayerAddress,
+        query as `0x${string}`,
+      ],
+    })
+
+    const txReceipt = await relayer.sendTransaction({
+      gasLimit: 500000,
+      speed: "fast",
+      to: axiomV1QueryAddress,
+      // 0.01 ETH
+      value: "10000000000000000",
+      data,
+    })
+
+    const tx = await client.waitForTransactionReceipt({
+      hash: txReceipt.hash as `0x${string}`,
+    })
+
+    if (tx.status === "reverted") {
+      return new Response("Error sending the query", { status: 500 })
+    }
+
+    return new Response(
+      JSON.stringify({
+        blockNumber,
+        keccakQueryResponse,
+        txHash: txReceipt.hash,
+      }),
+      { status: 200 }
+    )
+  } catch (e) {
+    console.error(e)
+    return new Response("An Error ocurred while sending the query", {
+      status: 500,
+    })
+  }
+}
--- apps/site/app/api/axiom/read-query-result/route.ts
+++ apps/site/app/api/axiom/read-query-result/route.ts
@@ -1,43 +1,48 @@
-import { ax } from "..";
-import { END_BLOCK, START_BLOCK, UNI_V3_POOL_ADDR } from "../.";
+import { ax } from ".."
+import { UNI_V3_POOL_ADDR } from "../."
 
-
-export async function GET(){
-
+export async function GET() {
   try {
-      const keccakQueryResponse = "0x0ed3360bc2b0a25033651c9d6bbe2b1a1e19ea721e8f9f67506f5f06b6829add";
+    const keccakQueryResponse =
+      "0x3e24c4c99e71ede2e621cd757f52c17221d270964b85b45bee48385802e51a82"
 
-    const responseTree = await ax.query.getResponseTreeForKeccakQueryResponse(keccakQueryResponse);
-    
-    const keccakBlockResponse = responseTree.blockTree.getHexRoot();
-    const keccakAccountResponse = responseTree.accountTree.getHexRoot();
-    const keccakStorageResponse = responseTree.storageTree.getHexRoot();
+    const responseTree =
+      await ax.query.getResponseTreeForKeccakQueryResponse(keccakQueryResponse)
 
-    const validationWitnessStart = ax.query.getValidationWitness(responseTree,START_BLOCK, UNI_V3_POOL_ADDR, 8)
-    const validationWitnessEnd = ax.query.getValidationWitness(responseTree,END_BLOCK, UNI_V3_POOL_ADDR, 8)
-    
-    if (!validationWitnessStart || !validationWitnessEnd) return new Response("Validation Witness not found", { status: 500 });
-       
+    const keccakBlockResponse = responseTree.blockTree.getHexRoot()
+    const keccakAccountResponse = responseTree.accountTree.getHexRoot()
+    const keccakStorageResponse = responseTree.storageTree.getHexRoot()
+
+    const validationWitness1 = ax.query.getValidationWitness(
+      responseTree,
+      9848630,
+      UNI_V3_POOL_ADDR,
+      8
+    )
+
+    if (!validationWitness1)
+      return new Response("Validation Witness not found", { status: 500 })
+
     const responseData = {
-    keccakBlockResponse,
-    keccakAccountResponse,
-    keccakStorageResponse,
-    blockResponses: [],
-    accountResponses: [
-      validationWitnessStart.accountResponse,
-      validationWitnessEnd.accountResponse,
-    ],
-    storageResponses: [
-      // validationWitnessStart.storageResponse,
-      // validationWitnessEnd.storageResponse,
-    ],
-  };
+      keccakBlockResponse,
+      keccakAccountResponse,
+      keccakStorageResponse,
+      blockResponses: [validationWitness1.blockResponse],
+      accountResponses: [],
+      storageResponses: [
+        validationWitness1.storageResponse,
+        // validationWitness2.storageResponse,
+        // validationWitness3.storageResponse,
+      ],
+    }
 
-  
-    return new Response(JSON.stringify({success: true, responseData}), { status: 200 })
-
+    return new Response(JSON.stringify({ success: true, responseData }), {
+      status: 200,
+    })
   } catch (e) {
-    console.error(e);
-    return new Response("An Error ocurred while sending the query", { status: 500 });
+    console.error(e)
+    return new Response("An Error ocurred while sending the query", {
+      status: 500,
+    })
   }
 }
