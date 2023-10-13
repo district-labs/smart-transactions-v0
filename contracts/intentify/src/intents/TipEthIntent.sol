f3011595016d4c79314f9cdc3eac73bbceaacd02
Kames Geraghty <sudo@kames.me>
2023-10-13 14:52:18 -0400

    Feat: Execute transaction from intent module (#54)
    * feat: execute transaction from intent
    
    * chore: fix CI test
    
    * chore: remove console2
    
    ---------
    
    Co-authored-by: Vitor <marthendalnunes@gmail.com>

---
+++ contracts/intentify/src/intents/TipEthIntent.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.19;
+
+import { Intent, Hook } from "../TypesAndDecoders.sol";
+import { ExecuteRootTransaction } from "./utils/ExecuteRootTransaction.sol";
+
+contract TipEthIntent is ExecuteRootTransaction {
+    constructor(address _intentifySafeModule) ExecuteRootTransaction(_intentifySafeModule) { }
+
+    function encode(uint256 amount) public pure returns (bytes memory) {
+        return abi.encode(amount);
+    }
+
+    function encodeHook(address target) public pure returns (bytes memory) {
+        return abi.encode(target);
+    }
+
+    function execute(Intent calldata intent, Hook calldata hook) external returns (bool) {
+        require(intent.root == msg.sender, "TipEthIntent:invalid-root");
+        require(intent.target == address(this), "TipEthIntent:invalid-target");
+        (uint256 amount) = abi.decode(intent.data, (uint256));
+        return executeFromRoot(hook.target, amount, new bytes(0));
+    }
+}
