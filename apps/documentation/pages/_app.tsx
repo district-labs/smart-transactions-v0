fa3471db6378eb60d7dc692daa566047965560e6
Kames Geraghty <sudo@kames.me>
2023-10-16 12:25:14 -0400

    WIP: Documentation (#57)
    * wip: docs
    
    * wip: docs
    
    * fix: lock
    
    * fix: remove apps/docs from cache
    
    * feat: data hub slides
    
    * fix: submodules

---
+++ apps/documentation/pages/_app.tsx
@@ -0,0 +1,10 @@
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'react-image-gallery/styles/css/image-gallery.css';
+import './styles.css'
+ 
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
