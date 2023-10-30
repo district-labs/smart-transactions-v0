1980137c591d6aa547c6c0d88c7adcb198df440e
Kames <sudo@kames.me>
2023-10-30 09:45:23 -0400

    wip: intentify api

--- apps/api/src copy/controllers/auth/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './auth-siwe-nonce'
-export * from './auth-siwe-sign-in'
-export * from './auth-siwe-sign-out'
--- apps/api/src copy/routes/auth.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import * as intentBatchUserController from "../controllers/auth";
-const router = Router();
-router.get("/nonce", intentBatchUserController.authSiweNonce);
-router.post("/sign-in", intentBatchUserController.authSiweSignIn);
-router.get("/sign-out", intentBatchUserController.authSiweSignOut);
-export default router;
