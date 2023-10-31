8ed4d8e63d990a7d8611456ab7da45dbcf9cf513
Vitor <marthendalnunes@gmail.com>
2023-10-31 08:32:55 -0300

    feat: add deploy scripts

--- contracts/intentify/script/deploy/CoreDeploy.s.sol
+++ contracts/intentify/script/deploy/CoreDeploy.s.sol
@@ -5,13 +5,14 @@ import "forge-std/Script.sol";
 import { IntentifySafeModule } from "../../src/module/IntentifySafeModule.sol";
 import { IntentifySafeModuleBundler } from "../../src/module/IntentifySafeModuleBundler.sol";
 import { WalletFactory } from "../../src/WalletFactory.sol";
+import { ERC20RebalanceIntent } from "../../src/intents/ERC20RebalanceIntent.sol";
 import { ERC20LimitOrderIntent } from "../../src/intents/ERC20LimitOrderIntent.sol";
 import { BlockNumberIntent } from "../../src/intents/BlockNumberIntent.sol";
 import { TimestampIntent } from "../../src/intents/TimestampIntent.sol";
 import { UniswapV3TwapIntent } from "../../src/intents/UniswapV3TwapIntent.sol";
 
 contract CoreDeploy is Script {
-    function run(address intentifySafeModule) external {
+    function run(address intentifySafeModule, address multisend) external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
         // Periphery Contracts
@@ -22,6 +23,7 @@ contract CoreDeploy is Script {
         new IntentifySafeModuleBundler();
 
         // Intent Modules Contracts
+        new ERC20RebalanceIntent(intentifySafeModule,multisend);
         new ERC20LimitOrderIntent(intentifySafeModule);
         new BlockNumberIntent();
         new TimestampIntent();
