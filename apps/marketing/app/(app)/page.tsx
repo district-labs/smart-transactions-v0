616327dec4201803ffda6cc3f7cd51e2c7483de1
Kames <sudo@kames.me>
2023-11-02 14:22:12 -0400

    fix: build

--- apps/marketing/app/(app)/page.tsx
+++ apps/marketing/app/(app)/page.tsx
@@ -5,9 +5,9 @@ import { strategies } from "@/data/strategies"
 import { Button } from "@district-labs/ui-react"
 import Balancer from "react-wrap-balancer"
 
-import { CardStrategyPreview } from "@/components/strategies/card-strategy-preview"
-import { LinkComponent } from "@/components/shared/link-component"
 import { siteConfig } from "@/config/site"
+import { LinkComponent } from "@/components/shared/link-component"
+import { CardStrategyPreview } from "@/components/strategies/card-strategy-preview"
 
 export default function Home() {
   return (
@@ -20,37 +20,37 @@ export default function Home() {
 }
 
 const SectionHero = () => {
-return (<section
-  id="hero"
-  aria-labelledby="hero-heading"
-  className="px-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:px-8 lg:py-20"
->
-  <div className="grid lg:grid-cols-12 gap-x-10 max-w-screen-2xl mx-auto">
-    <div className="col-span-5 flex flex-col justify-center">
-        <h1 className="text-5xl font-bold leading-tight tracking-normal md:text-5xl lg:text-8xl lg:leading-[1.1]">
-      <Balancer>
-          Build Onchain Wealth
-      </Balancer>
-        </h1>
-      <Balancer className="my-2 max-w-3xl text-lg text-muted-foreground sm:text-xl">
-        Create a portfolio of automated trading strategies without writing
-        a line of code.
-      </Balancer>
-      <div className="">
-        <LinkComponent href={siteConfig.links.app}>
-          <Button className="mt-4">Application</Button>
-        </LinkComponent>
+  return (
+    <section
+      id="hero"
+      aria-labelledby="hero-heading"
+      className="px-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:px-8 lg:py-20"
+    >
+      <div className="mx-auto grid max-w-screen-2xl gap-x-10 lg:grid-cols-12">
+        <div className="col-span-5 flex flex-col justify-center">
+          <h1 className="text-5xl font-bold leading-tight tracking-normal md:text-5xl lg:text-8xl lg:leading-[1.1]">
+            <Balancer>Build Onchain Wealth</Balancer>
+          </h1>
+          <Balancer className="my-2 max-w-3xl text-lg text-muted-foreground sm:text-xl">
+            Create a portfolio of automated trading strategies without writing a
+            line of code.
+          </Balancer>
+          <div className="">
+            <LinkComponent href={siteConfig.links.app}>
+              <Button className="mt-4">Application</Button>
+            </LinkComponent>
+          </div>
+        </div>
+        <div className="col-span-7 hidden lg:block">
+          <img
+            alt="Preview of District Finance Account"
+            src={"/images/preview-account.png"}
+            className="rounded-2xl border-white shadow-xl"
+          />
+        </div>
       </div>
-    </div>
-    <div className="col-span-7 hidden lg:block">
-      <img
-        alt="Preview of District Finance Account"
-        src={"/images/preview-account.png"}
-        className="rounded-2xl border-white shadow-xl"
-      />
-    </div>
-  </div>
-</section>)
+    </section>
+  )
 }
 const SectionAvailableStrategies = () => {
   return (
@@ -70,9 +70,9 @@ const SectionAvailableStrategies = () => {
         })}
       </div>
       <div className="container mt-10 text-center">
-      <LinkComponent href={siteConfig.links.app}>
-        <Button className="mt-4">Visit Application</Button>
-      </LinkComponent>
+        <LinkComponent href={siteConfig.links.app}>
+          <Button className="mt-4">Visit Application</Button>
+        </LinkComponent>
       </div>
     </section>
   )
@@ -82,7 +82,7 @@ const SectionHowItWorks = () => {
   return (
     <section>
       <div className="container mt-20">
-        <div className='grid gap-x-10 gap-y-32 lg:grid-cols-12'>
+        <div className="grid gap-x-10 gap-y-32 lg:grid-cols-12">
           <div className="order-2 col-span-6 lg:order-1">
             <img
               alt="Preview of Intent Batch"
@@ -91,11 +91,9 @@ const SectionHowItWorks = () => {
             />
           </div>
           <div className="order-1 col-span-6 flex flex-col justify-center lg:order-2">
-              <h1 className="text-5xl font-bold leading-tight tracking-normal md:text-5xl lg:text-8xl lg:leading-[1.1]">
-            <Balancer>
-                Smart Transactions
-            </Balancer>
-              </h1>
+            <h1 className="text-5xl font-bold leading-tight tracking-normal md:text-5xl lg:text-8xl lg:leading-[1.1]">
+              <Balancer>Smart Transactions</Balancer>
+            </h1>
             <Balancer className="my-2 max-w-3xl text-lg text-muted-foreground sm:text-xl">
               District Finance lets users express transactions as a series of
               steps. Each step can be a trade, a transfer, or a call to a smart
@@ -106,20 +104,19 @@ const SectionHowItWorks = () => {
             </div>
           </div>
         </div>
-        <div className='grid gap-x-10 gap-y-32 lg:grid-cols-12 mt-48'>
+        <div className="mt-48 grid gap-x-10 gap-y-32 lg:grid-cols-12">
           <div className="order-2 col-span-6">
             <img
-            alt="Preview of Intent Module"
+              alt="Preview of Intent Module"
               src={"/images/preview-code.png"}
               className="rounded-2xl border-white shadow-xl"
             />
           </div>
           <div className="order-1 col-span-6 flex flex-col justify-center">
-            
-              <h1 className="text-5xl font-bold leading-tight tracking-normal md:text-5xl lg:text-8xl lg:leading-[1.1]">
+            <h1 className="text-5xl font-bold leading-tight tracking-normal md:text-5xl lg:text-8xl lg:leading-[1.1]">
               <Balancer>Modular Rules & Conditions</Balancer>
-              </h1>
-            
+            </h1>
+
             <Balancer className="my-2 max-w-3xl text-lg text-muted-foreground sm:text-xl">
               At each step, users can specify rules and conditions that must be
               met before the step can be executed. This allows users to create
