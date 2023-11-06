0425069920802e4407af71dac7b728dc74a42b77
Kames <sudo@kames.me>
2023-11-06 09:58:03 -0500

    wip: deployment scripts for testnet

--- contracts/intentify/script/deploy/testnet/PeripheryDeploy.s.sol
+++ contracts/intentify/script/deploy/testnet/PeripheryDeploy.s.sol
@@ -2,15 +2,33 @@
 pragma solidity >=0.8.19;
 
 import "forge-std/Script.sol";
+import { UniswapV3TwapOracle } from "../../../src/periphery/Axiom/UniswapV3TwapOracle.sol";
 import { ERC20Mintable } from "../../../src/periphery/ERC20Mintable.sol";
+import { ChainlinkOracleTestnet } from "../../../src/periphery/ChainlinkOracleTestnet.sol";
 import { WalletFactoryTestnet } from "../../../src/WalletFactoryTestnet.sol";
 
 contract PeripheryDeploy is Script {
-    function run() external {
+    function run(address axiomV1QueryAddress) external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        // Zero-Knowlege Data Hub
+        new UniswapV3TwapOracle(axiomV1QueryAddress);
+
+        // ERC20 Tokens
         ERC20Mintable weth = new ERC20Mintable("Test Wrapped ETH","WETH",18);
         ERC20Mintable usdc = new ERC20Mintable("Test USDC","USDC", 6);
         ERC20Mintable dis = new ERC20Mintable("District Labs","DIS", 18);
         ERC20Mintable rizz = new ERC20Mintable("Web3 Rizz","RIZZ", 18);
+
+        // Chainlink Oracles
+        int256 wethPrice = 2000e8;
+        int256 usdcPrice = 1e8;
+        int256 disPrice = 5e8;
+        int256 rizzPrice = 10e8;
+        new ChainlinkOracleTestnet(wethPrice);
+        new ChainlinkOracleTestnet(usdcPrice);
+        new ChainlinkOracleTestnet(disPrice);
+        new ChainlinkOracleTestnet(rizzPrice);
 
         address[] memory addressList = new address[](4);
         addressList[0] = address(weth);
@@ -22,11 +40,13 @@ contract PeripheryDeploy is Script {
         amountList[1] = 10_000e6;
         amountList[2] = 5e18;
         amountList[3] = 1e18;
+        vm.stopBroadcast();
 
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        uint256 deployerPrivateKey = vm.envUint("WALLET_FACTORY_PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        WalletFactoryTestnet walletFactory = new WalletFactoryTestnet(
+        // Wallet Factory
+        new WalletFactoryTestnet(
             addressList,
             amountList);
 
