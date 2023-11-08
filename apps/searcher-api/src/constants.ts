e7e86efb15bdb47e73d2286dbec8cc0ccea12225
Vitor <marthendalnunes@gmail.com>
2023-11-08 17:50:56 -0300

    fix: searcher env variables

--- apps/searcher-api/src/constants.ts
+++ apps/searcher-api/src/constants.ts
@@ -1,15 +1,14 @@
 import { createPublicClient, http, type Address } from "viem"
 import { goerli } from "viem/chains"
+import { env } from "./env"
 
 export const SUPPORTED_CHAINS = [5, 31337]
-export const MULTICALL_WITH_FLASHLOAN_ADDRESS =
-  "0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb"
 export const INTENTIFY_API_URL = "http://localhost:3002"
 
 export function getSearcherAddressBychainId(chainId: number): Address {
   switch (chainId) {
     case 5: {
-      return "0xf84B4AbfcC1E062aa54D738f2ABE00c1B85090DF"
+      return env.SEARCHER_ADDRESS_GOERLI
     }
     default: {
       throw new Error(`Unsupported chainId: ${chainId}`)
--- apps/searcher-api/src/env.ts
+++ apps/searcher-api/src/env.ts
@@ -1,4 +1,5 @@
 import { createEnv } from "@t3-oss/env-core"
+import { isAddress } from "viem"
 import { z } from "zod"
 
 export const env = createEnv({
@@ -6,10 +7,13 @@ export const env = createEnv({
     GOERLI_RPC_URL: z.string().url(),
     PORT: z.string().default("3000"),
     CORS_ORIGIN: z.string(),
+    INTENTIFY_API_URL: z.string().url(),
     OPEN_ZEPPELIN_DEFENDER_API_KEY_MAINNET: z.string().length(32),
     OPEN_ZEPPELIN_DEFENDER_SECRET_KEY_MAINNET: z.string().length(64),
     OPEN_ZEPPELIN_DEFENDER_API_KEY_GOERLI: z.string().length(32),
     OPEN_ZEPPELIN_DEFENDER_SECRET_KEY_GOERLI: z.string().length(64),
+    MULTICALL_WITH_FLASHLOAN_ADDRESS: z.string().refine(isAddress),
+    SEARCHER_ADDRESS_GOERLI: z.string().refine(isAddress),
   },
   runtimeEnv: process.env,
   emptyStringAsUndefined: true,
