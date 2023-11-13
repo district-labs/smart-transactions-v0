7a06e6c1e5b94d5d725687183f972bf0af96ddcb
Vitor <marthendalnunes@gmail.com>
2023-11-13 18:03:51 -0300

    fix: update cron time to send axiom to 30 minutes

--- apps/api-cron/src/cron-jobs/axiom-query/send-axiom-query-uni-v3-goerli.ts
+++ apps/api-cron/src/cron-jobs/axiom-query/send-axiom-query-uni-v3-goerli.ts
@@ -3,10 +3,10 @@ import "dotenv/config";
 import { sendAxiomQueryUniV3Goerli } from "../../actions/axiom-query/send-axiom-query-uni-v3-goerli";
 
 /**
- * Cron job to Send Axiom queries every 5 minutes
+ * Cron job to Send Axiom queries every 30 minutes
  */
 export const jobSendAxiomQueryUniV3Goerli = new CronJob(
-  "0 */5 * * * *", // cronTime
+  "0 */30 * * * *", // cronTime
   async function () {
     console.log("Sending Axiom queries");
     await sendAxiomQueryUniV3Goerli();
