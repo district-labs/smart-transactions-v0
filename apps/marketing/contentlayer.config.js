3dbbbaa4273e17be0e0144618e0b851dfcf5668b
justin <justinbassey@gmail.com>
2023-11-13 16:17:21 +0000

    feat: updated welcome to district blog post

--- apps/marketing/contentlayer.config.js
+++ apps/marketing/contentlayer.config.js
@@ -40,15 +40,6 @@ export const Post = defineDocumentType(() => ({
       type: "string",
       required: true,
     },
-    authors: {
-      // Reference types are not embedded.
-      // Until this is fixed, we can use a simple list.
-      // type: "reference",
-      // of: Author,
-      type: "list",
-      of: { type: "string" },
-      required: true,
-    },
   },
   computedFields,
 }))
