e7beeaa8ede25ef89fc4c5f89a980b8df3bf2005
Kames <sudo@kames.me>
2023-11-14 04:28:44 -0500

    feat: ping searcher api

--- apps/api-cron/src/server.ts
+++ apps/api-cron/src/server.ts
@@ -1,19 +1,33 @@
 import 'dotenv/config';
 import { CronJob } from 'cron';
 import { invalidateIntentBatches } from './actions/invalidate-intent-batches';
+import { pingSearcherEngine } from './actions/ping-searcher-engine';
 
 /**
  * Cron job to invalidate intents every 5 minutes
  */
 const jobInvalidateIntents = new CronJob(
-	'0 */5 * * * *', // cronTime
-	async function () {
-		console.log('Invalidating intents');
-    await invalidateIntentBatches();
-	}, // onTick
-	null, // onComplete
-	true, // start
+	'0 */5 * * * *',
+	async () => {
+		await invalidateIntentBatches();
+	},
+	() => console.log('Intent Batches Invalidated'), // onComplete
+	false, // start
 	'America/Los_Angeles' // timeZone
 );
 
+/**
+ * Cron job to ping searcher every 2 minutes
+ */
+const jobPingSearcher = new CronJob(
+	'0 */2 * * * *',
+	async () => {
+		await pingSearcherEngine();
+	},
+	() => console.log('Searcher Engine Pinged'), // onComplete
+	false, // start
+	'America/Los_Angeles' // timeZone
+);
+
+jobPingSearcher.start();
 jobInvalidateIntents.start();
