04cfbe4f7050eec663492bb0ef00ac36e52e2988
Kames Geraghty <sudo@kames.me>
2023-11-14 07:02:13 -0500

    Fix: Cron API URL routes (#121)
    * Fix: Nonce encoding and defaults (#117)
    
    * fix: api url (#115)
    
    * fix: searcher api url
    
    * fix: nonce encoding and defaults
    
    * Feat: Add Cron Job for Searcher Engine (#120)
    
    * fix: api url (#115)
    
    * fix: searcher api url
    
    * feat: ping searcher api
    
    * fix: .gitignore
    
    * fix: infinite loop limit order form
    
    * feat: fix auto-update nonce (#100)
    
    * fix: cron service routes

--- apps/api-cron/src/actions/ping-searcher-engine.ts
+++ apps/api-cron/src/actions/ping-searcher-engine.ts
@@ -2,7 +2,7 @@ import { env } from "../env";
 
 export async function pingSearcherEngine() {
     try {
-        await fetch(`${env.SEARCHER_API_URL}/engine`, {
+        await fetch(`${env.SEARCHER_API_URL}engine`, {
             method: 'GET',
         });
     } catch (error) {
